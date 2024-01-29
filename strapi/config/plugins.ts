c24abdcb5a0888e6e7bf122811b44b1057d1ce13
Jon Bernard <jon@jonbernard.net>
2024-01-29 12:32:59 -0500

    image uploads

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,5 +1,30 @@
-export default () => ({
-  "import-export-entries": {
-    enabled: true,
-  },
-});
+const getUploadCreds = (str: string) => {
+  const array = /cloudinary:\/\/(.*):(.*)@(.*)/g.exec(str);
+
+  return {
+    cloud_name: array[3],
+    api_key: array[1],
+    api_secret: array[2],
+  };
+};
+
+export default ({ env }) => {
+  const cloudinary = getUploadCreds(env("CLOUDINARY_URL"));
+
+  return {
+    "import-export-entries": {
+      enabled: true,
+    },
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: cloudinary,
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  };
+};
