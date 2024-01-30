711e6443c6564cf5c9a59d941dd070572eb1c2f3
Jon Bernard <jon@jonbernard.net>
2024-01-30 12:48:06 -0500

    icon updates

--- strapi/src/admin/app.tsx
+++ strapi/src/admin/app.tsx
@@ -1,10 +1,26 @@
+import favicon from "./extensions/favicon.png";
+import logo from "./extensions/logo.png";
+
 export default {
   config: {
+    auth: {
+      logo,
+    },
+    head: {
+      favicon: favicon,
+    },
+    menu: {
+      logo,
+    },
     translations: {
       en: {
         "app.components.LeftMenu.navbrand.title": "Phi Chapter",
+        "Auth.form.welcome.title": "Welcome to the admin!",
+        "Auth.form.welcome.subtitle": "Log in to the app",
       },
     },
   },
-  bootstrap() {},
+  bootstrap() {
+    document.title = "Admin | Phi Chapter of Phi Kappa Tau";
+  },
 };
